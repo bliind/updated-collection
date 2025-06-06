076fc417d741555aa964811816d27767fc1756d3
Mike Sweeney <blindx@gmail.com>
2025-06-06 10:31:23 -0400

    make better for mobile

--- src/parts/PlaceholderCard.jsx
+++ src/parts/PlaceholderCard.jsx
@@ -1,9 +1,9 @@
 function PlaceholderCard() {
     return (
         <div className="m-2 align-self-start">
-            <div className="card text-center position-relative" style={{ width: '16rem' }}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="12rem" height="16rem" className="mx-auto rounded-top placeholder-wave">
-                        <rect width="12rem" height="16rem" fill="#868e96"></rect>
+            <div className="card text-center position-relative variant-card">
+                <svg xmlns="http://www.w3.org/2000/svg" width="90%" height="100%" className="mx-auto rounded-top placeholder-wave">
+                        <rect width="100%" height="100%" fill="#868e96"></rect>
                     </svg>
                 <div className="card-body py-2 bg-darker rounded-bottom">
                     <h6 className="card-title my-0 py-0 fw-bold">
--- src/parts/VariantCard.jsx
+++ src/parts/VariantCard.jsx
@@ -5,30 +5,27 @@ function VariantCard({ card, setModalCard }) {
     return (
         <div className="m-2 align-self-start">
             <div
-                className="card text-center position-relative"
-                style={{ width: '16rem' }}
+                className="card text-center position-relative variant-card"
                 onClick={() => setModalCard(card)}
                 data-bs-toggle="modal"
-                data-bs-target="#mainModal">
-                <img
-                    loading="lazy"
-                    className="rounded-top card-img-top"
-                    src={imgSrc} />
+                data-bs-target="#mainModal"
+            >
+                <img loading="lazy" className="rounded-top card-img-top" src={imgSrc} />
 
                 <div className="card-body py-2 bg-darker rounded-bottom">
                     <h6 className="card-title my-0 py-0 fw-bold">{card.card_name}</h6>
                     <p className="card-text my-0 py-0">{card.variant_name ? card.variant_name : card.artist_name }</p>
                     {card.tags && (
-                        // <p className="card-text my-0 py-0 fst-italic">{card.tags.replaceAll(',', ' ')}</p>
                         <span
                             className={`${badgeStyle} bg-info text-dark fst-italic`}
                             style={{ top: '-0.5em', right: '-0.5em' }}
-                            >{card.tags.replaceAll(',', ' ')}</span>
+                        >{card.tags.replaceAll(',', ' ')}</span>
                     )}
                     {card.video_link ? '' : (
                         <span
                             className={`${badgeStyle} bg-warning text-dark fw-bold`}
-                            style={{ top: '-0.5em', left: '-0.5em' }}>NEEDED</span>
+                            style={{ top: '-0.5em', left: '-0.5em' }}
+                        >NEEDED</span>
                     )}
                 </div>
             </div>
